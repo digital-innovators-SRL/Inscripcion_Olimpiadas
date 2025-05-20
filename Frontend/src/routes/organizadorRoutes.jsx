e4ac5aef19229815a86046192863952ff8638f0f
Eth4nSP <eth4n.gerl@gmail.com>
2025-05-20 00:07:35 -0400

    Reinstalando mi front

--- Frontend/src/routes/organizadorRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import DashboardPage from '../pages/Organizador/DashboardPage'
-import ReportsPage from '../pages/Organizador/ReportsPage'
-import ProfilePage from '../pages/ProfilePage'
-
-export default [
-  { path: '/dashboard', element: <DashboardPage />, allowedRoles: ['Organizador'] },
-  { path: '/reports', element: <ReportsPage />, allowedRoles: ['Organizador'] },
-  { path: '/profile', element: <ProfilePage />, allowedRoles: ['Organizador'] },
-]
